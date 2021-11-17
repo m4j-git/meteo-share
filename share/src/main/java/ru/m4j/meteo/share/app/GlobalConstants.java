517b0b89ce272afd05630d9156141dc3cdc3e589
m4j <git@m4j.ru>
2021-11-18 02:16:13 +0300

    up global constants

--- share/src/main/java/ru/m4j/meteo/share/app/GlobalConstants.java
+++ share/src/main/java/ru/m4j/meteo/share/app/GlobalConstants.java
@@ -3,10 +3,16 @@
  */
 package ru.m4j.meteo.share.app;
 
+import java.io.File;
+
 public class GlobalConstants {
 
-    public static final String minTs = "1970-01-01 00:00:01";
-    public static final String maxTs = "2038-01-19 03:14:07";
+    private GlobalConstants() {
+
+    }
+
+    public static final String MIN_TS = "1970-01-01 00:00:01";
+    public static final String MAX_TS = "2038-01-19 03:14:07";
 
     // Regex for acceptable logins
     public static final String LOGIN_REGEX = "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
@@ -20,5 +26,7 @@ public class GlobalConstants {
     public static final String SPRING_PROFILE_PRODUCTION = "prod";
     public static final String SPRING_PROFILE_STAGE = "stage";
 
-    public static final String basePackages = "ru.m4j.meteo";
+    public static final String BASE_PACKAGES = "ru.m4j.meteo";
+
+    public static final String TEST_DATA_PATH = "src" + File.separator + "test" + File.separator + "data" + File.separator;
 }
